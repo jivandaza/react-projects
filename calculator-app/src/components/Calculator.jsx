432efac62a310fc0aab51f8e73933a565fc3cf3c
Jossid Daza <joser1.jidl@gmail.com>
2024-03-08 21:57:33 -0500

    Adding initial component structure

---
+++ calculator-app/src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../styles/calculator.css';
+
+const Calculator = () => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Calculator;
