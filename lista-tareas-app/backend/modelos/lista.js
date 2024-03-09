63e8666b3090779a40321e912b1b5c4c061ff465
Jossid Daza <joser1.jidl@gmail.com>
2024-03-09 15:25:12 -0500

    Modify Lista.js file

--- lista-tareas-app/backend/modelos/lista.js
+++ lista-tareas-app/backend/modelos/lista.js
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
 
-const listaSchema = new mongoose.Schema({
-   titulo: {
-       type: String,
-       required: true
-   },
-   estado: {
-       type: Boolean,
-       required: true
-   }
-});
+const listaSchema = new mongoose.Schema(
+    {
+        titulo: {
+            type: String,
+            required: true
+        },
+        estado: {
+            type: Boolean,
+            required: true
+        },
+        usuario: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: 'Usuario'
+            }
+        ]
+    },
+    { timestamps: true }
+);
 
 export default mongoose.model("Lista", listaSchema);
