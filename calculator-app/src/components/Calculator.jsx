4b3ba96a690973986e8f519f63c9014b12288cdd
Jossid Daza <joser1.jidl@gmail.com>
2024-03-08 22:21:48 -0500

    Add the button list to the component

--- calculator-app/src/components/Calculator.jsx
+++ calculator-app/src/components/Calculator.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './../styles/calculator.css';
 
+import { btns, BTN_ACTIONS } from "./btnConfig";
+
 const Calculator = () => {
     return(
-        <div>
+        <div className='calculator'>
+            <div className="calculator__result">
+                <div className="calculator__result__exp">
 
+                </div>
+                <div className="calculator__result__exp">
+
+                </div>
+            </div>
+            <div className="calculator__btns">
+                {
+                    btns.map((item, index) => (
+                        <button key={index} className={item.class}>
+                            {item.display}
+                        </button>
+                    ))
+                }
+            </div>
         </div>
     );
 }
