2da36c96ec5702baa4a02b8a28629694642662e2
Jossid Daza <joser1.jidl@gmail.com>
2024-03-10 09:34:17 -0500

    Testing user authentication API

--- lista-tareas-app/backend/app.js
+++ lista-tareas-app/backend/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import './db/conexion.js';
+import auth from './rutas/autenticacion.js';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo!');
-})
+});
+
+app.use('/api/v1', auth);
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en: http://localhost:${port}/`)
