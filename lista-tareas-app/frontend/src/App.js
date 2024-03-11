729b4836640d4f825ff569fcda093e458efbb3fb
Jossid Daza <joser1.jidl@gmail.com>
2024-03-11 11:02:24 -0500

    Customize Routes app

--- lista-tareas-app/frontend/src/App.js
+++ lista-tareas-app/frontend/src/App.js
@@ -1,13 +1,20 @@
 import Navbar from './componentes/navbar/Navbar';
 import Inicio from './componentes/inicio/Inicio';
+import Acerca from './componentes/acerca/Acerca';
 import Footer from './componentes/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
-            <Inicio />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={<Inicio />} />
+                    <Route path='/acerca' element={<Acerca />} />
+                </Routes>
+            </Router>
             <Footer />
         </div>
     );
