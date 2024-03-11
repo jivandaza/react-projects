a5325870aa85b2319ebe82267b44de05ff2c4e6d
Jossid Daza <joser1.jidl@gmail.com>
2024-03-11 09:59:27 -0500

    Create the About component

---
+++ lista-tareas-app/frontend/src/componentes/acerca/Acerca.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Acerca = () => {
+    return (
+        <div className='about d-flex justify-content-center align-items-center'>
+            <div className="conatiner">
+                <div className='d-flex justify-content-center align-items-center'>
+                    <h1>Acerca De</h1>
+                </div>
+                <p>
+                    Tareas MERN es una aplicación de gestión de tareas diseñada para ayudarte a organizar tu vida diaria de manera eficiente y mantener un control efectivo de tus responsabilidades. Esta aplicación ofrece una interfaz intuitiva y características poderosas para que puedas planificar, priorizar y completar tus tareas de manera efectiva.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Acerca;
