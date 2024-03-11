40758a13207a0e822cf2335ef69adf4c7dcf5a25
Jossid Daza <joser1.jidl@gmail.com>
2024-03-11 14:08:16 -0500

    Added acceso component

--- lista-tareas-app/frontend/src/App.js
+++ lista-tareas-app/frontend/src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './componentes/navbar/Navbar';
 import Inicio from './componentes/inicio/Inicio';
 import Acerca from './componentes/acerca/Acerca';
+import Registrarse from './componentes/registrarse/Registrarse';
+import Acceso from './componentes/acceso/Acceso';
 import Footer from './componentes/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
@@ -13,6 +15,8 @@ function App() {
                 <Routes>
                     <Route exact path='/' element={<Inicio />} />
                     <Route path='/acerca' element={<Acerca />} />
+                    <Route path='/registrarse' element={<Registrarse />} />
+                    <Route path='/acceso' element={<Acceso />} />
                 </Routes>
             </Router>
             <Footer />
