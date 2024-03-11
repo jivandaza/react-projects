9068f084237bf81e8454af415f935386675439fa
Jossid Daza <joser1.jidl@gmail.com>
2024-03-10 23:31:02 -0500

    Add the Navbar, Home and Footer components

--- lista-tareas-app/frontend/src/App.js
+++ lista-tareas-app/frontend/src/App.js
@@ -1,9 +1,14 @@
+import Navbar from './componentes/navbar/Navbar';
+import Home from './componentes/inicio/Home';
+import Footer from './componentes/footer/Footer';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-
+            <Navbar />
+            <Home />
+            <Footer />
         </div>
     );
 }
