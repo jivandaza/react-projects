6ec3ff3b997fcd3073c721dc93b7ae4f98f39211
Jossid Daza <joser1.jidl@gmail.com>
2024-03-14 13:58:48 -0500

    Created submit method

--- lista-tareas-app/frontend/src/componentes/acceso/Acceso.jsx
+++ lista-tareas-app/frontend/src/componentes/acceso/Acceso.jsx
@@ -1,10 +1,54 @@
 import './../../estilos/registrarse.css';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store";
 import Titulo from "../titulo/Titulo";
-import React from 'react';
+import React, {useState} from 'react';
 
 const Acceso = () => {
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const [Inputs, setInputs] = useState({correoFrm: "", contraseniaFrm: ""});
+    const change = (e) => {
+        const { name, value } = e.target;
+        setInputs({ ...Inputs, [name]: value });
+    }
+    const submit = async (e) => {
+        e.preventDefault();
+        // en el hosting en el metodo post se incluye: ${window.location.origin}
+        await axios
+            .post(`http://localhost:3001/api/v1/acceso`, Inputs)
+            .then((response) => {
+                const { message, err, user } = response.data;
+
+                if ( err ) {
+                    toast.warning(message);
+                } else {
+                    toast.success(message);
+                    sessionStorage.setItem('id', user._id);
+                    dispatch(authActions.login());
+                    setInputs({correoFrm: '', contraseniaFrm: ''});
+                    setTimeout(() => {
+                        history('/tareas');
+                    }, 3000);
+                }
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+
+                if ( status === 400) {
+                    toast.error(data.message);
+                } else if ( status === 500 ) {
+                    toast.error(data.message);
+                }
+            });
+    }
     return (
         <div className='signup'>
+            <ToastContainer />
             <div className="container">
                 <div className="row">
                     <div className="col-lg-4 vh-100 d-none border-end border-1 border-black d-lg-flex justify-content-center align-items-center">
@@ -18,16 +62,23 @@ const Acceso = () => {
                             <input
                                 className='p-2 mb-3 input-signup'
                                 type='email'
-                                name='correo'
-                                placeholder='Introduce tu correo electrónico'
+                                name='correoFrm'
+                                placeholder='Correo electrónico'
+                                onChange={change}
+                                value={Inputs.correoFrm}
                             />
                             <input
                                 className='p-2 mb-3 input-signup'
                                 type='password'
-                                name='contrasenia'
-                                placeholder='Introduce tu contraseña'
+                                name='contraseniaFrm'
+                                placeholder='Contraseña'
+                                onChange={change}
+                                value={Inputs.contraseniaFrm}
                             />
-                            <button className='p-2 btn-signup'>Acceder</button>
+                            <button
+                                className='p-2 btn-signup'
+                                onClick={submit}
+                            >Acceder</button>
                         </div>
                     </div>
                 </div>
