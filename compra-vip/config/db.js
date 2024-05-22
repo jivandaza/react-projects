4dfff5905f04b9a62c450cbf76f642146435a3c0
Jossid Daza <joser1.jidl@gmail.com>
2024-05-22 10:42:25 -0500

    Compra VIP version 1.8.9

--- compra-vip/config/db.js
+++ compra-vip/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect('mongodb+srv://jivandaza:1IVYAOJFuymeG7to@cluster0.ekcyzsi.mongodb.net/compra-vip');
     } catch (err) {
         console.error(err);
     }
