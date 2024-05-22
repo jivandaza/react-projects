cf779e98e4353e3f808a184643a890d295c1e6e8
Jossid Daza <joser1.jidl@gmail.com>
2024-05-22 15:59:33 -0500

    Compra VIP version 1.9.3

--- compra-vip/controller/user/userLogout.js
+++ compra-vip/controller/user/userLogout.js
@@ -1,10 +1,7 @@
 const userLogoutController = async (req, res) => {
     try {
         res.clearCookie('token', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
-            path: '/'
+            expires: new Date(0)
         });
 
         res.json({
